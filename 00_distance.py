21392a741b0f93af8c94fd4fc29e437e7362ed6f
1eeroyJenkins <84621720+1eeroyJenkins@users.noreply.github.com>
2023-02-25 11:54:52 +0400

    Выполнил задачу из файла distance + удалил файл probe

--- 00_distance.py
+++ 00_distance.py
@@ -3,6 +3,7 @@
 
 # Есть словарь координат городов
 
+from pprint import pprint
 sites = {
     'Moscow': (550, 370),
     'London': (510, 510),
@@ -12,12 +13,32 @@ sites = {
 # Составим словарь словарей расстояний между ними
 # расстояние на координатной сетке - корень из (x1 - x2) ** 2 + (y1 - y2) ** 2
 
+moscow = sites['Moscow']
+london = sites['London']
+paris = sites['Paris']
+
 distances = {}
 
-# TODO здесь заполнение словаря
+msk_lndn = ((moscow[0] - london[0])**2 + (moscow[1]-london[1])**2)**.5
+msk_prs = ((moscow[0] - paris[0])**2 + (moscow[1]-paris[1])**2)**.5
+lndn_prs = ((london[0] - paris[0])**2 + (london[1]-paris[1])**2)**.5
 
+# TODO здесь заполнение словаря
+distances['MSK'] = {'London':msk_lndn, 'Paris':msk_prs}
+distances['LNDN'] = {'Moscow': msk_lndn, 'Paris':lndn_prs}
+distances['PRS'] = {'Moscow':msk_prs, 'London': lndn_prs}
 print(distances)
 
 
+dist = {}
+dist['Moscow'] = {}
+dist['Moscow']["London"] = msk_lndn
+dist['Moscow']['Paris'] = msk_prs
+dist['London'] = {}
+dist['London']['Moscow'] = msk_lndn
+dist['London']['Paris'] = lndn_prs
+dist['Paris'] = {}
+dist['Paris']['Moscow'] = msk_prs
+dist['Paris']['London'] = msk_lndn
 
-
+pprint(dist)
